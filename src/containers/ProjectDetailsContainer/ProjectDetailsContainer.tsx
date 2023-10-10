6530f45f66c6fadacb220625813b8ac756770c79
Maël-Thi <mseme@hotmail.fr>
2023-10-10 18:27:11 +0200

    feat(project-etape): add select to be able to change etape of project
    * I would have made a unique select for the filter and the project details page with more time

--- src/containers/ProjectDetailsContainer/ProjectDetailsContainer.tsx
+++ src/containers/ProjectDetailsContainer/ProjectDetailsContainer.tsx
@@ -1,15 +1,41 @@
+import { ETAPE } from "../../constants/project"
+import { usePatchProjectById } from "../../hooks/usePatchProject"
+
 export const ProjectDetailsContainer = ({
   description,
   commentaire,
   etape,
+  id,
 }: APIProject) => {
+  const { mutate } = usePatchProjectById(id)
+  const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedEtape = e.target.value
+    mutate({ etape: selectedEtape as ETAPE })
+  }
+
   return (
     <>
       <div className="flex justify-between">
         <p className="text-1xl py-1 w-2/3">{description}</p>
-        <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-base font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-          {etape}
-        </span>
+        <select
+          className="rounded-md bg-red-50 px-2 py-1 text-base font-medium text-red-700 ring-1 ring-inset ring-red-600/10"
+          name="etapes"
+          id="etapes"
+          onChange={handleFilterChange}
+        >
+          <option
+            selected={etape === ETAPE.EN_ATTENTE}
+            value={ETAPE.EN_ATTENTE}
+          >
+            {ETAPE.EN_ATTENTE}
+          </option>
+          <option selected={etape === ETAPE.EN_COURS} value={ETAPE.EN_COURS}>
+            {ETAPE.EN_COURS}
+          </option>
+          <option selected={etape === ETAPE.TERMINE} value={ETAPE.TERMINE}>
+            {ETAPE.TERMINE}
+          </option>
+        </select>
       </div>
       <h3 className="mt-10 text-xl font-semibold">Commentaires</h3>
       <ul>
