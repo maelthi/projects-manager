b22b27334ea2dcbb304e4e9bef6f996128189549
Maël-Thi <mseme@hotmail.fr>
2023-10-10 19:27:25 +0200

    feat(project): write doc and home message

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,9 @@
 export const Home = () => {
-  return <p className="text-3xl font-bold underline"></p>
+  return (
+    <div className="relative w-full md:max-w-2xl md:mx-auto text-center mt-10">
+      <h1 className="font-bold text-gray-700 text-xl sm:text-2xl md:text-5xl leading-tight mb-6">
+        Welcome to your new projects manager !!
+      </h1>
+    </div>
+  )
 }
